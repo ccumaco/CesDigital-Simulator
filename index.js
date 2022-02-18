097e7c7efe5e4cfba4609f756ea0e6d8b884a758
ccumaco <carloscumaco5@gmail.com>
2022-02-18 13:38:39 -0500

    Validations-form

--- index.js
+++ index.js
@@ -17,31 +17,52 @@ function openMenuMobile(state) {
 let modalFormIndex = false;
 function modalFormContacUs(){
     modalFormIndex = !modalFormIndex;
-    if (modalFormIndex) {
-        document.querySelector('.modal-to-pay').style.display = "flex";
-    } else {
-        document.querySelector('.modal-to-pay').style.display = "none";
+    if (validateNumber() && validateName() && validateEmail()) {   
+        if (modalFormIndex) {
+            document.querySelector('.modal-to-pay').style.display = "flex";
+        } else {
+            document.querySelector('.modal-to-pay').style.display = "none";
+        }
+        document.querySelector('.modal-to-pay').innerHTML = `
+        <div class="content-modal">
+            <span class="close-modal-to-pay" onclick="modalFormContacUs()">&times;</span>
+            <img class="image-checked" src="./images/checked.png" />
+            <h2 class="title-modal">Por último, déjanos tus datos para poder contactarnos contigo</h2>
+        </div>
+        `
+        setTimeout(() => {
+            document.querySelector('#form-contact-us').submit()
+        }, 3000);
     }
-    document.querySelector('.modal-to-pay').innerHTML = `
-    <div class="content-modal">
-        <span class="close-modal-to-pay" onclick="modalFormContacUs()">&times;</span>
-        <img class="image-checked" src="./images/checked.png" />
-        <h2 class="title-modal">Por último, déjanos tus datos para poder contactarnos contigo</h2>
-    </div>
-    `
-    setTimeout(() => {
-        document.querySelector('#form-contact-us').submit()
-    }, 3000);
 }
 
 openMenuMobile(false);
 
+function validateNumber(){
+    if (document.getElementById('subject').value.length > 2 && document.getElementById('subject').value.length < 30) {
+        return true
+    }
+    return false
+}
+function validateName(){
+    if (document.getElementById('name').value.length > 2 && document.getElementById('name').value.length < 30) {
+        return true
+    }
+    return false
+}
 
+function validateEmail(){
+    let emailRegex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+    if (emailRegex.test(document.getElementById('mail').value)){
+        return true
+    }
+    return false
+}
 
-
-function validateFormContactUs(){
-    let nameInput = document.getElementById('name').value
-    if (nameInput != null || '' && nameInput.lenght >= 5) {
-        
+function confirmSendForm () {
+    if (validateName() && validateNumber() && validateEmail()) {
+        document.querySelector('.thanks-modal').style.display = 'block'
+        document.querySelector('.container-hidden').style.display = 'none'
     }
 }
+
--- simulator.js
+++ simulator.js
@@ -252,33 +252,7 @@ function initialPriceService(service){
 }
 
 
-function validateNumber(){
-    if (document.getElementById('subject').value.length > 2 && document.getElementById('subject').value.length < 30) {
-        return true
-    }
-    return false
-}
-function validateName(){
-    if (document.getElementById('name').value.length > 2 && document.getElementById('name').value.length < 30) {
-        return true
-    }
-    return false
-}
 
-function validateEmail(){
-    let emailRegex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
-    if (emailRegex.test(document.getElementById('mail').value)){
-        return true
-    }
-    return false
-}
-
-function confirmSendForm () {
-    if (validateName() && validateNumber() && validateEmail()) {
-        document.querySelector('.thanks-modal').style.display = 'block'
-        document.querySelector('.container-hidden').style.display = 'none'
-    }
-}
 
 let modalToPay = false
 function openModalToPay(){
